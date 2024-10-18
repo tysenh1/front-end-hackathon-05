b03b02a65aa45d3f6f8aa31bc1c030a88aa67aa3
SimonW <mwang8@academic.rrc.ca>
2024-10-17 23:02:19 -0500

    add new function to validate form

--- scripts/meng-script.js
+++ scripts/meng-script.js
@@ -1,4 +1,9 @@
-document.addEventListener("DOMContentLoaded", addButton(), addColumn(), hiddenFeedback());
+document.addEventListener("DOMContentLoaded", function() {
+    addButton();
+    addColumn(); 
+    hiddenFeedback(); 
+    hideError();
+})
 
 function addButton(){
     const pricerows = document.querySelectorAll('.mochaprice table tr');
@@ -76,3 +81,54 @@ function showFeedback(){
         document.getElementById("feedbackMessage").style.display="none";
     }
 }
+
+function hideError(){
+    const cafeerror = document.getElementsByClassName("cafeerror")
+    for(i=0; i<cafeerror.length; i++ )
+    cafeerror[i].style.display="none";
+}
+
+document.getElementById("cafeform").addEventListener("submit", validate);
+function validate(e){
+    if (showErrors()){
+        e.preventDefault();
+        return false
+    };
+    return true;
+}
+
+function showErrors(){
+    let errorFlag=false;
+    if(!radioSelect()){
+        document.getElementById("rate_error").style.display = "block"
+        errorFlag=true
+    }
+    if(!dropdownSelect()){
+        document.getElementById("product_error").style.display = "block"
+        errorFlag=true
+    }
+    return errorFlag
+}
+
+function radioSelect(){
+    const rate = document.getElementsByName("caferate")
+    for(i=0; i<rate.length; i++){
+        if(rate[i].checked){
+            return true;
+        }
+    }
+    return false
+}
+
+function dropdownSelect(){
+    let product = document.getElementById("cafeproduct").value.trim()
+    return product !== ""
+}
+
+function clearErrors(errorId){
+    let error = document.getElementById(errorId);
+    error.style.display = "none";
+}
+
+document.getElementById("caferadio").addEventListener("input", function(){clearErrors("rate_error")})
+document.getElementById("cafeproduct").addEventListener("input", function(){clearErrors("product_error")})
